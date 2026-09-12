02afebdc21db71a16a6f73010af63b3651b91744
kamilink7 <ethanthomas1309@outlook.com>
2026-09-11 19:44:53 -0700

    Fixed setting placement to allow recording mode to override

--- src/f_ap/f_ap_game.cpp
+++ src/f_ap/f_ap_game.cpp
@@ -751,6 +751,14 @@ static void duskExecute() {
     updateAutoSave();
     auto floatMusicVolume = static_cast<float>(dusk::getSettings().audio.mainMusicVolume.getValue()) / 100;
 
+    if (floatMusicVolume != 1.0f) {
+        Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(floatMusicVolume, 0);
+        Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(floatMusicVolume, 0);
+    } else {
+        Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(1.0f, 0);
+        Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(1.0f, 0);
+    }
+
     if (dusk::getSettings().game.recordingMode) {
         Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(0.0f, 0);
         Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(0.0f, 0);
@@ -759,14 +767,6 @@ static void duskExecute() {
         Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(1.0f, 0);
         Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(1.0f, 0);
         isRecording = false;
-    }
-
-    if (floatMusicVolume != 1.0f) {
-        Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(floatMusicVolume, 0);
-        Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(floatMusicVolume, 0);
-    } else {
-        Z2GetSoundMgr()->getSeqMgr()->getParams()->moveVolume(1.0f, 0);
-        Z2GetSoundMgr()->getStreamMgr()->getParams()->moveVolume(1.0f, 0);
     }
 
     if (dusk::getSettings().game.fastSpinner && mDoCPd_c::getHoldR(PAD_1)) {
