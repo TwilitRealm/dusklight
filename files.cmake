fad89f18a3c8e520c49da1d70ea3df07dd1a9754
Luke Street <luke@street.dev>
2026-09-15 23:55:50 -0600

    Mod manager updates & dropdown component

--- files.cmake
+++ files.cmake
@@ -1584,6 +1584,8 @@ set(DUSK_FILES
         src/dusk/ui/document.hpp
         src/dusk/ui/drop_install_modal.cpp
         src/dusk/ui/drop_install_modal.hpp
+        src/dusk/ui/dropdown_button.cpp
+        src/dusk/ui/dropdown_button.hpp
         src/dusk/ui/editor.cpp
         src/dusk/ui/editor.hpp
         src/dusk/ui/event.cpp
