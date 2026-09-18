94eb7bd8e7147079e39f902c7cefafdd4dc8bee9
baxlek <115663151+baxlek@users.noreply.github.com>
2026-09-18 12:51:04 -0500

    Super clawshot options draft

--- src/dusk/settings.h
+++ src/dusk/settings.h
@@ -314,7 +314,8 @@ struct UserSettings {
         ConfigVar<bool> infiniteRupees;
         ConfigVar<bool> enableIndefiniteItemDrops;
         ConfigVar<bool> moonJump;
-        ConfigVar<SuperClawshotMode> superClawshot;
+        ConfigVar<bool> superClawshotAttach;
+        ConfigVar<bool> superClawshotLength;
         ConfigVar<AlwaysGreatspinMode> alwaysGreatspin;
         ConfigVar<bool> enableFastIronBoots;
         ConfigVar<bool> canTransformAnywhere;
